f9a7c8ec77af022ccbecdecb41879d9fa34bfe8f
Adrian Rivera Luna <riveralu@esat-alumni.com>
2022-09-22 18:26:24 +0200

    premake

---
+++ tools/Premake/premake5.lua
@@ -0,0 +1,47 @@
+workspace "ArteLuna"
+    configurations { "Debug", "Release"}
+    location "../../ArteLuna"
+project "ArteLuna"
+    location "../../ArteLuna/ArteLuna"
+    kind "ConsoleApp"
+    language "C++"
+    targetdir "../../bin/"
+
+    -- Ficheros
+    files {"**.h" , "**.c"}
+
+    -- Linkeado
+
+    links { "glfw3.lib", "glfw3dll.lib", "kernel32.lib", "user32.lib", "gdi32.lib", "winspool.lib", "comdlg32.lib", "advapi32.lib", "shell32.lib", "ole32.lib", "oleaut32.lib", "uuid.lib", "odbc32.lib", "odbccp32.lib", "%(AdditionalDependencies)" }
+    libdirs { "../../deps/GLFW/lib-vc2019","%(AdditionalLibraryDirectories)" }
+
+    -- Filtros
+    filter "configurations:Debug"
+    defines { "DEBUG" }
+
+    filter "configurations:Release"
+    defines { "NDEBUG" }
+
+    filter "src"
+    defines {"src"}
+    files {"src/**.cpp"}
+
+    filter "include"
+    defines {"include"}
+    files {"include/**.h"}
+
+    filter "bin"
+    defines {"bin"}
+    files {"include/**.exe"}
+
+    filter "deps"
+    defines {"deps"}
+    files {"deps/GLFW/include/**.h"}
+
+    filter "tools"
+    defines {"tools"}
+
+    filter "examples"
+    defines {"examples"}
+
+    filter {}
