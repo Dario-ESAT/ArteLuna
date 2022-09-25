ecea9fc8854d9593799dc577471ac429196a7de4
Delliv <riveralu@esat-alumni.com>
2022-09-25 21:05:01 +0200

    name of some functions

--- include/windowClass.h
+++ include/windowClass.h
@@ -15,11 +15,11 @@ class Window{
     ~Window();
 
     //Parameters
-    int16_t get_width();
-    void set_width();
-    int16_t get_height();
-    void set_height();
-    void set_windowed();
+    int16_t GetWidth();
+    void SetWidth();
+    int16_t GetHeight();
+    void SetHeight();
+    void SetWindowed();
 
     //Functions
     int Init(const char* name, int16_t width = 1280, int16_t heigth = 720, Vector2 pos = {110.0f, 110.0f}, bool windowed = true, int monitor = 0);
