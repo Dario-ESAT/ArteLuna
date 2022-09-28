895ee681981f13e77443b5d8a398d417baf8306f
Delliv <riveralu@esat-alumni.com>
2022-09-28 17:28:32 +0200

    starting triangle

---
+++ include/structs.h
@@ -0,0 +1,9 @@
+#ifndef __STRUCTS_H__
+#define __STRUCTS_H__ 1
+
+struct Vtx {
+	float x, y, z;
+	float r, g, b;
+};
+
+#endif
--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,77 @@
 #include "window.h"
+#include "glad/gl.h"
+#include "structs.h"
 #include <stdio.h>
 
 
+GLuint buffer_ = 0;
+static GLuint gShaderProgram = 0;
+static GLuint gVBO = 0, gVAO = 0;
+static GLuint gEBO = 0;
+
+Vtx vertices[] = {
+    { -0.5f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f},
+    {  0.5f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f},
+    {  0.0f, 1.0f, 0.0f,    1.0f, 0.0f, 1.0f}
+};
+
+int indices[] = { 0,1,2, 2,3,0 };
+
+
+void onInit()
+{
+    //gladLoadGL();
+    // ---------------------------Shaders-------------------------------
+    //char* vertex_shader = (char*)Slurp("./vertex.glslv"); Sustituir con lo de glfw 
+    unsigned int vertex_ = glCreateShader(GL_VERTEX_SHADER);
+    //glShaderSource(vertex_, 1, &vertex_shader, 0);
+    glCompileShader(vertex_);
+
+    //char* fragment_shader = (char*)Slurp("./fragment.glslf");
+    unsigned int fragment_ = glCreateShader(GL_FRAGMENT_SHADER);
+    //glShaderSource(fragment_, 1, &fragment_shader, 0);
+    glCompileShader(fragment_);
+    // -----------------------------------------------------------------
+
+    // ---------------------------Program-------------------------------
+    gShaderProgram = glCreateProgram();
+    glAttachShader(gShaderProgram, vertex_);
+    glAttachShader(gShaderProgram, fragment_);
+    glLinkProgram(gShaderProgram);
+
+    glDeleteShader(vertex_);
+    glDeleteShader(fragment_);
+    // -----------------------------------------------------------------
+
+    // ----------------------------Mesh---------------------------------
+    glGenVertexArrays(1, &gVAO);
+    glGenBuffers(1, &gVBO);
+    glGenBuffers(1, &gEBO);
+
+    glBindVertexArray(gVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, gVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, gEBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vtx), 0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vtx), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindVertexArray(0);
+    // -----------------------------------------------------------------
+}
+
+// -----------------------------------------------------------------------------------------------------------
+
+void onFrame()
+{
+    glUseProgram(gShaderProgram);
+    glBindVertexArray(gVAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
 
 int main() {
     printf("Hello World");
