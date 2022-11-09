fbcb3b8347408d9d003b58e85665d2062753afe4
Dario-ESAT <artilesdi@esat-alumni.es>
2022-11-09 16:52:29 +0100

    added getters in entity

--- include/entity.h
+++ include/entity.h
@@ -1,9 +1,13 @@
 #ifndef __ENTITY_H__
 #define __ENTITY_H__ 1
 
+#include <optional>
+#include <vector>
+
 #include "stdint.h"
 #include "vector_3.h"
 #include "matrix_3.h"
+#include "components/component.h"
 
 
 // para las listas de los componentes hacerlas de std::optional<componente>
@@ -13,18 +17,32 @@ public:
 
     Entity();
     ~Entity();
-    uint32_t getId() const;
+    uint32_t id() const;
 
+    const mathlib::Vector3& position() const;
+    void set_position(const mathlib::Vector3& position);
+
+    const mathlib::Vector3& rotation() const;
+    void set_rotation(const mathlib::Vector3& rotation);
+    
+    const mathlib::Vector3& scale() const;
+    void set_scale(const mathlib::Vector3& scale);
+
+    bool dirty() const;
+    
+    void set_components(const std::vector<std::optional<Component>>& components);
+
+    const mathlib::Matrix3x3& transform();
+
+protected:
+    bool dirty_;
+    uint32_t id_;
+    std::vector<std::optional<Component>> components_;
+    
     mathlib::Vector3 position_;
     mathlib::Vector3 rotation_;
     mathlib::Vector3 scale_;
-
-    const mathlib::Matrix3x3& transform();
-private:
-    bool dirty_;
     mathlib::Matrix3x3 transform_;
-    uint32_t id_;
-  
 };
 
 
--- src/entity.cc
+++ src/entity.cc
@@ -12,3 +12,47 @@ Entity::~Entity() {
 
 }
 
+uint32_t Entity::id() const {
+    return id_;
+}
+
+const mathlib::Vector3& Entity::position() const {
+    return position_;
+}
+
+void Entity::set_position(const mathlib::Vector3& position) {
+    position_ = position;
+    dirty_ = true;
+}
+
+const mathlib::Vector3& Entity::rotation() const {
+    return rotation_;
+}
+
+void Entity::set_rotation(const mathlib::Vector3& rotation) {
+    rotation_ = rotation;
+    dirty_ = true;
+}
+
+const mathlib::Vector3& Entity::scale() const {
+    return scale_;
+}
+
+void Entity::set_scale(const mathlib::Vector3& scale) {
+    scale_ = scale;
+    dirty_ = true;
+}
+
+bool Entity::dirty() const {
+    return dirty_;
+}
+
+void Entity::set_components(
+    const std::vector<std::optional<Component>>& components) {
+    components_ = components;
+}
+
+const mathlib::Matrix3x3& Entity::transform() {
+    return transform_;
+}
+
