01e644902f3da07e0e28e44f21efcf363a44ae67
Dario-ESAT <artilesdi@esat-alumni.es>
2022-11-29 18:40:39 +0100

    fixed crash

--- include/engine/entity_manager.h
+++ include/engine/entity_manager.h
@@ -2,14 +2,13 @@
 #define __ENTITY_MANAGER_H__ 1
 #include <optional>
 #include <vector>
-
+#include "entity.h"
 #include "components/render_component.h"
 #include "components/rigid_body_component.h"
 #include "components/tranform_component.h"
 #include "components/sound_emiter_component.h"
 #include "components/sound_listener_component.h"
 
-class Entity;
 
 class EntityManager {
     public:
@@ -20,13 +19,11 @@ class EntityManager {
 private:
     std::vector<Entity> entities_;
     
-    std::vector<std::pair<std::optional<RenderComponent>,
-    std::optional<TranformComponent>>> hola;
-    std::vector<RenderComponent> render_components_;
+    std::vector<std::optional<RenderComponent>> render_components_;
+    std::vector<std::optional<TranformComponent>> tranform_components_;
     // std::vector<RigidBodyComponent> rigid_body_components_;
     // std::vector<SoundEmiterComponent> sound_emiter_components_;
     // std::vector<SoundListenerComponent> sound_listener_components_;
-    std::vector<TranformComponent> tranform_components_;
     
     EntityManager();
     ~EntityManager();
