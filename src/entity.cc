0eca1a2ba19291d11329b2fb92ad8f2f853942b2
Adrian Rivera Luna <riveralu@esat-alumni.com>
2022-12-21 17:22:25 +0100

    reworked get component

--- src/entity.cc
+++ src/entity.cc
@@ -1,7 +1,9 @@
 #define GLFW_INCLUDE_NONE
 
 #include "entity.h"
+#include "engine/entity_manager.h"
 #include "components/transform_component.h"
+#include "components/render_component.h"
 #include "components/component.h"
 
 Entity::Entity() {
@@ -29,4 +31,16 @@ uint32_t Entity::id() const {
     return id_;
 }
 
+
+TransformComponent* Entity::get_transform_component() {
+    return & EntityManager::GetManager().transform_components_[id_];
+}
+
+
+RenderComponent* Entity::get_render_component() {
+    if (EntityManager::GetManager().render_components_[id_].has_value()) {
+        return & EntityManager::GetManager().render_components_[id_].value();
+    }
+    return nullptr;
+}
 
