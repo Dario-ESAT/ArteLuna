60cd5f3a1e88abfdc2b09d97addcfcad0560ec4c
Adrian Rivera Luna <riveralu@esat-alumni.com>
2023-02-13 17:57:42 +0100

    starting material settings

---
+++ deps/arteluna/include/engine/material_settings.h
@@ -0,0 +1,26 @@
+#ifndef H_MATERIAL_SETTINGS_
+#define H_MATERIAL_SETTINGS_ 1
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "shader.h"
+#include "program.h"
+#include "texture.h"
+
+class MaterialSettings {
+  
+public:
+  MaterialSettings();
+  ~MaterialSettings();
+  
+  virtual void use() = 0;
+  virtual void SetUniformTime();
+
+private:
+ 
+};
+
+
+#endif
