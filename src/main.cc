a0b21e2b3863296c756cc4bda6aadeeb1ffb2f73
Adrian <ariveralun@gmail.com>
2023-05-28 21:37:56 +0200

    fixed some things

--- src/main.cc
+++ src/main.cc
@@ -18,9 +18,7 @@ int main() {
 
   al::Window& window = *engine.CreateNewWindow("Aleksander",1600,900);
   window.camera_.set_position({0,2.5f,-5.f});
-  al::LightManager l_manager(em,
-  "../../deps/arteluna/bin/shadow_render.glslv",
-  "../../deps/arteluna/bin/shadow_render.glslf");
+  al::LightManager l_manager(em);
   sm.Add(l_manager);
   assert(sm.Get<al::LightManager>());
   window.camera_.InitCubeMap();
